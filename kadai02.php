dceaec06ff63e403149f1016339d5e6f3b31c124
itoyu <yuuuuu0412@frontier.hokudai.ac.jp>
2019-06-12 23:49:51 +0900

    修正

--- kadai02.php
+++ kadai02.php
@@ -3,7 +3,7 @@ $a = 3;
 $b = 7;
 echo $a + $b;
 $array_month = ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"];
-echo $array_month[8];
+echo $array_month[7];
 $hello = "Hello,";
 $name = "Yu";
 $world = "'s World!";
