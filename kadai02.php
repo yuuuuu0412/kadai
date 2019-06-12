e20338637369713c4767bfa62bc6e15ebf7e2ddb
itoyu <yuuuuu0412@frontier.hokudai.ac.jp>
2019-06-12 21:43:05 +0900

    課題提出

---
+++ kadai02.php
@@ -0,0 +1,31 @@
+<?php
+$a = 3;
+$b = 7;
+echo $a + $b;
+$array_month = ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"];
+echo $array_month[8];
+$hello = "Hello,";
+$name = "Yu";
+$world = "'s World!";
+echo $hello . $name . $world;
+$tech_boost = "tech";
+$tech_boost .= " boost";
+//
+$calendar_2018 = [
+  "January" => "1月",
+  "February" => "2月",
+  "March" => "3月",
+  "April" => "4月",
+  "May" => "5月",
+  "June" => "6月",
+  "July" => "7月",
+  "August" => "8月",
+  "September" => "9月",
+  "October" => "10月",
+  "November" => "11月",
+  "December" => "12月"
+];
+// 12月を表示する
+echo $calendar_2018["December"];
+
+ ?>
